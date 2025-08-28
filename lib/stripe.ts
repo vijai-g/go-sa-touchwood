66911fc5782466847383132ecfb85a910e5e8d17
vijai-g <vijai.g94@gmail.com>
2025-08-29 02:52:07 +0530

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,2 +1,12 @@
-import Stripe from 'stripe'
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
+// lib/stripe.ts
+import Stripe from 'stripe';
+
+// Keep it simple: don't pin apiVersion. Use account default when/if enabled.
+// Export a lazy getter so we don't construct without a key in COD mode.
+export function getStripe() {
+  const key = process.env.STRIPE_SECRET_KEY;
+  if (!key) {
+    throw new Error('Stripe disabled: STRIPE_SECRET_KEY not set');
+  }
+  return new Stripe(key);
+}
