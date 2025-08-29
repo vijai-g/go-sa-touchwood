4fa198a1524721655fb49c164c3455618333223f
vijai-g <vijai.g94@gmail.com>
2025-08-29 18:16:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverActions: { bodySizeLimit: '2mb' } },
+  env: {
+    NEXT_PUBLIC_ASSET_VERSION:
+      process.env.VERCEL_GIT_COMMIT_SHA || `${Date.now()}`
+  }
 };
 module.exports = nextConfig;
