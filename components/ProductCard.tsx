1594c5e8198cc21b631c1a7b3372189b160dbb71
vijai-g <vijai.g94@gmail.com>
2025-09-04 18:12:03 +0530

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -37,7 +37,7 @@ export function ProductCard({ product }: { product: Product }) {
             src={src}
             alt={product.name}
             fill
-            className="object-contain object-center p-2"
+            className="object-contain object-center"
             sizes="(min-width:1024px) 33vw, (min-width:640px) 50vw, 100vw"
             priority={false}
           />
